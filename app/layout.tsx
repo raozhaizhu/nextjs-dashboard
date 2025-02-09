dbfc0446668286b8ae077a7bde41db1c9ce80115
raozhaizhu <raozhaizhu@gmail.com>
2025-02-10 00:23:07 +0800

    finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Rzz Dashboard',
+        default: 'Rzz Dashboard',
+    },
+    description: 'The Next.js Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -8,3 +18,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </html>
     );
 }
+
